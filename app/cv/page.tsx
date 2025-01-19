2f37d19f0f1ec81da094b6e1ccbd902fef74e107
Michael Lemiale <michael.lemiale@gmail.com>
2025-01-19 18:35:10 +0100

    cv updates, icon update

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -5,6 +5,8 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import { useState } from 'react';
 import { Document, Page as PDFPage, pdfjs } from 'react-pdf';
 import { ArrowIcon } from '../components/icons'
+import 'react-pdf/dist/Page/TextLayer.css';
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -21,7 +23,7 @@ export default function Page(fileName) {
     return (
         <section>
             <a
-                className="text-xl flex items-center transition-all hover:text-red-400 mb-12"
+                className="text-xl flex items-center transition-all hover:text-red-400 mb-16"
                 rel="noopener noreferrer"
                 download
                 target="_blank"
@@ -30,11 +32,19 @@ export default function Page(fileName) {
                 <ArrowIcon />
                 <p className="ml-2 h-7">Download CV</p>
             </a>
-            <Document file="/michael-lemiale-resume-2025.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-                {Array.from(new Array(numPages), (el, index) => (
-                    <PDFPage key={`page_${index + 1}`} pageNumber={index + 1} />
-                ))}
-            </Document>
+            <div className="border-4 border-solid border-red-400 w-fit">
+                <Document file="/michael-lemiale-resume-2025.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                    {Array.from(new Array(numPages), (el, index) => (
+                        <PDFPage 
+                            key={`page_${index + 1}`} 
+                            pageNumber={index + 1}
+                            scale={1.1}               
+                            renderAnnotationLayer={false}
+                            renderTextLayer={false}
+                        />
+                    ))}
+                </Document>
+            </div>
         </section>
     )
 }
